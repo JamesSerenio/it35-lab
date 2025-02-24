5940c225e3ef275534b4cae2be7febddc3e55a57
JamesSerenio <sereniojames363@gmail.com>
2025-02-24 08:16:39 +0800

    added search bar in search.tsx

--- src/pages/home-tabs/Search.tsx
+++ src/pages/home-tabs/Search.tsx
@@ -1,39 +1,34 @@
 import { 
-    IonButtons,
-      IonContent, 
-      IonHeader, 
-      IonMenuButton, 
-      IonPage, 
-      IonTitle, 
-      IonToolbar 
-  } from '@ionic/react';
-  
-  const Search: React.FC = () => {
-    return (
+  IonButtons,
+  IonContent, 
+  IonHeader, 
+  IonMenuButton, 
+  IonPage, 
+  IonSearchbar, 
+  IonTitle, 
+  IonToolbar 
+} from '@ionic/react';
+
+const Search: React.FC = () => {
+  return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot='start'>
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>Search</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-          <div
-            style={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            Search
-          </div>
-  
-        </IonContent>
+          <IonHeader>
+              <IonToolbar>
+                  <IonButtons slot='start'>
+                      <IonMenuButton></IonMenuButton>
+                  </IonButtons>
+                  <IonTitle>Search</IonTitle>
+              </IonToolbar>
+          </IonHeader>
+          <IonContent fullscreen>
+              <IonSearchbar placeholder="Search..."></IonSearchbar>
+              <IonSearchbar placeholder="Custom Placeholder"></IonSearchbar>
+              <IonSearchbar disabled={true} placeholder="Disabled"></IonSearchbar>
+              <IonSearchbar value="Value"></IonSearchbar>
+              <IonSearchbar animated={true} placeholder="Animated"></IonSearchbar>
+          </IonContent>
       </IonPage>
-    );
-  };
-  
-  export default Search;
+  );
+};
+
+export default Search;

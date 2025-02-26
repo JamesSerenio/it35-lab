289d9ee0c53dda7beb65fd8c652f1dd71728668d
JamesSerenio <sereniojames363@gmail.com>
2025-02-26 10:23:25 +0800

    aaded about_me.tsx and css

--- src/pages/home-tabs/About_me.tsx
+++ src/pages/home-tabs/About_me.tsx
@@ -1,28 +1,60 @@
 import { 
-  IonButtons,
-  IonContent, 
-  IonHeader, 
-  IonMenuButton, 
-  IonPage, 
-  IonTitle, 
-  IonToolbar 
+    IonButtons,
+    IonContent, 
+    IonHeader, 
+    IonMenuButton, 
+    IonPage, 
+    IonTitle, 
+    IonToolbar 
 } from '@ionic/react';
+import "../../styles/AboutMe.css";
+const AboutMe: React.FC = () => {
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonButtons slot="start">
+                        <IonMenuButton />
+                    </IonButtons>
+                    <IonTitle>About Me</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <section className="about fade-in" id="About">
+                    <div className="row fade-in">
+                        <div className="info fade-in">
+                            <h3><span> Name:</span> James M. Serenio </h3>
+                            <h3><span> Age: </span> 22 </h3>
+                            <h3><span> Post:</span> Front End Developer, Web Designer, Programmer, Web Developer </h3>
+                            <h3><span> Language: </span> English </h3>
+                        </div>
+                        
+                        <div className="counter fade-in">
+                            <div className="box fade-in">
+                                <span>5+</span>
+                                <h3>Project Completed</h3>
+                            </div>
 
-const Search: React.FC = () => {
-  return (
-      <IonPage>
-          <IonHeader>
-              <IonToolbar>
-                  <IonButtons slot='start'>
-                      <IonMenuButton></IonMenuButton>
-                  </IonButtons>
-                  <IonTitle>About Me</IonTitle>
-              </IonToolbar>
-          </IonHeader>
-          <IonContent fullscreen>
-          </IonContent>
-      </IonPage>
-  );
+                            <div className="box fade-in">
+                                <span>2+</span>
+                                <h3>Years Of Studying</h3>
+                            </div>
+
+                            <div className="box fade-in">
+                                <span>5+</span>
+                                <h3>Certificate Awards</h3>
+                            </div>
+
+                            <div className="box fade-in">
+                                <span>5+</span>
+                                <h3>Satisfied Customers</h3>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </IonContent>
+        </IonPage>
+    );
 };
 
-export default Search;
+export default AboutMe;

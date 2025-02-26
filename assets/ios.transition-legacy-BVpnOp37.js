6078f7d57cbd2064c9e6c0018bf1b58880b351a0
JamesSerenio <sereniojames363@gmail.com>
2025-02-26 20:37:47 +0800

    Updates

--- assets/ios.transition-legacy-BVpnOp37.js
+++ assets/ios.transition-legacy-BVpnOp37.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-DD1i4ddp.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
+System.register(["./index-legacy-BYOpe9-Y.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.m,n=t.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

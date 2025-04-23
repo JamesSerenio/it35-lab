886f61bac5da031f10c0f59098e73748c291a6f4
JamesSerenio <sereniojames363@gmail.com>
2025-04-23 19:01:59 +0800

    added some code for register,feed and feedContainer

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://coorcmzlxazalpklbjak.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvb3JjbXpseGF6YWxwa2xiamFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwOTIxODQsImV4cCI6MjA1NjY2ODE4NH0.-wTKsfOEQJXdeOmzafnTiOyyQ0MD9E7Dk0d5CkS7DDE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
